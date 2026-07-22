e3c9ee0246169f0974ebb76caac6469927d16d46
Bolaji Simon <netforge27@gmail.com>
2026-07-22 10:57:48 +0100

    the first task

---
+++ async.js
@@ -0,0 +1,22 @@
+
+async function fetchRes() {
+    try {
+        let controller = new AbortController()
+        let res = await fetch("https://jsonplaceholder.typicode.com/posts")
+        setTimeout(() => {
+            if (!res) {
+                controller.abort()
+            }
+        }, 2000);
+        let data = await res.json()
+        let postsTotal = 0
+        data.forEach(data => {
+            postsTotal ++
+        });
+        console.log(postsTotal)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+fetchRes()
