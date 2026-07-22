6807e2b59f7b83a9e89778ab48f54ca80e5c7073
Bolaji Simon <netforge27@gmail.com>
2026-07-22 11:00:28 +0100

    added the error mess

--- async.js
+++ async.js
@@ -5,7 +5,7 @@ async function fetchRes() {
         let res = await fetch("https://jsonplaceholder.typicode.com/posts")
         setTimeout(() => {
             if (!res) {
-                controller.abort()
+                controller.abort('Request was cacnelled')
             }
         }, 2000);
         let data = await res.json()
